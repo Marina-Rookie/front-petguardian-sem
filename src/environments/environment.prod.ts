1776e12dfe2f33913b100fcc9a6d94d52a433801
Marina.p <125839912+Marina-Rookie@users.noreply.github.com>
2025-03-02 00:34:49 -0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //url_server: 'http://localhost:3000/api/',
-  url_server: 'https://api-petguardian.vercel.app/api/',
+  url_server: 'http:back-petguardian-sem.vercel.app',
   secret_key: 'petguardianpass'
 };
